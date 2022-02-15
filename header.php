2fb72cdef0b6dfde4bcc623159c598fde6e7b047
shamim-pro <md.shamimtpi@gmail.com>
2022-02-16 00:47:34 +0600

    header login/my-account button added

--- header.php
+++ header.php
@@ -35,39 +35,38 @@ if ( function_exists('wp_body_open') ) {
     <?php $header_layout = get_post_meta( get_the_ID(), 'dostart-header-status', true ); ?>
     <?php if ( 'disabled' !== $header_layout ) { ?>
     <header class="dostart-header-area">
-        <div class="dostart-main-header">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3 logo_col">     
-                        <div class="site-logo">
-                            <?php
-                            if ( has_custom_logo() ) {
-                                 the_custom_logo();
-                            } ?>
-                            <div class="site-text-logo">
-                                <?php if ( is_front_page() && is_home() ) :  ?>
-                                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php echo esc_html(bloginfo('name')); ?></a></h1>
-                                    <?php
-                                else :
-                                    ?>
-                                <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php echo esc_html(bloginfo('name')); ?></a></h1>
-                                <?php  endif;?>
-                            </div>
-                        </div>                            
-                    </div>
-                      <div class="col-md-9 menu_col">
-                      <div class="primary_header">
-                         <a href="#" class="toggle"><span></span></a>
-                         <div class="dostart-mainmenu">
-                              <nav class="nav-menu" id="site-navigation">
-                                 <?php
+        <div class="container">
+            <div class="row" style="align-items:center">
+                <div class="col-md-2 logo_col">     
+                    <div class="site-logo">
+                        <?php
+                        if ( has_custom_logo() ) {
+                                the_custom_logo();
+                        } ?>
+                        <div class="site-text-logo">
+                            <?php if ( is_front_page() && is_home() ) :  ?>
+                            <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php echo esc_html(bloginfo('name')); ?></a></h1>
+                                <?php
+                            else :
+                                ?>
+                            <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php echo esc_html(bloginfo('name')); ?></a></h1>
+                            <?php  endif;?>
+                        </div>
+                    </div>                            
+                </div>
+                <div class="col-md-10 menu_col">
+                    <div class="primary_header">
+                        <a href="#" class="toggle"><span></span></a>
+                        <div class="dostart-mainmenu">
+                            <nav class="nav-menu" id="site-navigation">
+                                <?php
                                     $theme_location = 'primary';
                                     if ( has_nav_menu($theme_location) ) {
                                         wp_nav_menu(
                                             array(
-												'theme_location' => $theme_location,
-												'menu_id' => 'primary-menu',
-												'walker'  => new Dostart_Nav_Walker(),
+                                                'theme_location' => $theme_location,
+                                                'menu_id' => 'primary-menu',
+                                                'walker'  => new Dostart_Nav_Walker(),
                                             )
                                         );
                                     }else {
@@ -80,18 +79,33 @@ if ( function_exists('wp_body_open') ) {
                                     
                                     }
                                     ?>
-                              </nav>
+                            </nav>
                         </div>
-                        
-                        <!-- Woocommere Cart Icon -->
-                        <?php if ( function_exists('dostart_header_cart') && class_exists('WooCommerce') ) { ?>
+                    </div>
+
+
+                    <div class="dostart-wc-button">
+                    <!-- Woocommere Cart Icon -->
+                    <?php if ( function_exists('dostart_header_cart') && class_exists('WooCommerce') ) { ?>
                             <div class="dostart-header-cart" >
                                 <?php dostart_header_cart(); ?>
                             </div>  
-                        <?php } ?>
+                    <?php } ?>
+
+                    <div class="dostart-my-account-btn">
+                        <a href="<?php echo esc_url( wc_get_page_permalink( 'myaccount' ) ); ?>">
+                            <?php
+                            if ( is_user_logged_in() ) { ?>
+                            <span><?php   esc_html_e('My Account', 'digicart'); ?></span>
+                        <?php } else {  ?>
+                            <span><?php   esc_html_e('Login', 'digicart'); ?></span>
+                            <?php } ?>
+                        </a>
+                    </div> <!-- End my account button. -->
                     </div>
-                    </div>
+
                 </div>
+
             </div>
         </div>
     </header> 
