f258aca134e519c3590d3988c0db83fe2aea92f3
shamim-pro <md.shamimtpi@gmail.com>
2022-02-17 18:45:50 +0600

    Update hooks.php

--- inc/hooks.php
+++ inc/hooks.php
@@ -1,13 +1,48 @@
-<?php
 
-remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
-remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
-remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
-remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
-remove_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display' );
+<?php 
+
+
+
+
+add_filter( 'comment_form_fields', 'digicart_comment_field_to_bottom' );
+
+/**
+ * Comment Field to Bottom
+ * @param $fields
+ * @return mixed
+ */
+function digicart_comment_field_to_bottom( $fields ) {
+	$comment_field = $fields['comment'];
+	unset( $fields['comment'] );
+	$fields['comment'] = $comment_field;
+	return $fields;
+}
+
+
+/**
+ * Hide Dostart Theme Notice.
+ */
+add_action( 'wp_ajax_dostart_admin_notice_ajax_object_save', 'dostart_admin_notice_ajax_object_callback' );
+
+function dostart_admin_notice_ajax_object_callback() {
+   
+
+	$data = isset($_POST['data']) ? sanitize_text_field(wp_unslash($_POST['data'])) : array();
+	if ( $data ) {
+		
+		// Check valid request form user.
+		check_ajax_referer('dostart_theme_notice_status');
+	
+		update_option('dostart_theme_notice', $data);  
+
+		$response['message'] = 'sucess';
+		wp_send_json_success($response);
+	}
+
+	wp_die();
+
+}
+
+
+
+?>
