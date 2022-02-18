8eca8809eb9039590291f1c4a84576c78848e770
shamim-pro <md.shamimtpi@gmail.com>
2022-02-18 21:27:52 +0600

    Update woocommerce.php

--- inc/class/woocommerce.php
+++ inc/class/woocommerce.php
@@ -40,20 +40,6 @@ add_action( 'after_setup_theme', 'dostart_woocommerce_setup' );
 function dostart_woocommerce_scripts() {
 
 	wp_enqueue_style( 'dostart-woocommerce-style', get_template_directory_uri() . '/assets/css/woocommerce.min.css' );
-
-	$font_path   = WC()->plugin_url() . '/assets/fonts/';
-	$inline_font = '@font-face {
-			font-family: "star";
-			src: url("' . $font_path . 'star.eot");
-			src: url("' . $font_path . 'star.eot?#iefix") format("embedded-opentype"),
-				url("' . $font_path . 'star.woff") format("woff"),
-				url("' . $font_path . 'star.ttf") format("truetype"),
-				url("' . $font_path . 'star.svg#star") format("svg");
-			font-weight: normal;
-			font-style: normal;
-		}';
-
-	wp_add_inline_style( 'dostart-woocommerce-style', $inline_font );
 }
 add_action( 'wp_enqueue_scripts', 'dostart_woocommerce_scripts' );
 
